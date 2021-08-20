85804ac62058fc76bb04b974d31f94803f6fecc2
Jack Shendrikov <jackshendrikov@gmail.com>
2021-08-20 13:40:07 +0300

    Added project implementation with the help of REST API + Add home page

--- dj_test/settings.py
+++ dj_test/settings.py
@@ -23,8 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'rest_framework',
 
     'api',
+    'rest',
 ]
 
 MIDDLEWARE = [
@@ -112,5 +114,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
