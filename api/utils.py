3ce1e1dbaa56fd3d59e3ebdeb4b9ffc890019b6c
Jack Shendrikov <jackshendrikov@gmail.com>
2021-08-20 12:50:17 +0300

    Add new endpoint which outputs the request records filtered by current IP

--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,29 @@
+import hashlib
+
 import dns.resolver
 import dns.exception
 
 from re import match
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
+from api.models import UserRequest
+
+
+def save_request(ip, params):
+    add_request = UserRequest(
+        ip=ip,
+        email=params['email'],
+        password=hash_password(params['password']),
+        first_name=params['first_name'],
+        last_name=params['last_name']
+    )
+    add_request.save()
+
+
+def hash_password(password):
+    encoded_info = password.encode()
+    hasher = hashlib.sha256(encoded_info)
+    return hasher.hexdigest()
 
 
 def validate_email_domain(email):
@@ -12,6 +32,8 @@ def validate_email_domain(email):
 
     if domain in BANNED_DOMAINS:
         raise ValidationError(_('You cannot use \'%(domain)s\' domain'), params={'domain': domain},)
+    elif UserRequest.objects.filter(email=email).exists():
+        raise ValidationError(_('This mail is already in use'), code='invalid')
     else:
         try:
             dns.resolver.resolve(domain, 'MX')
--- api/views.py
+++ api/views.py
@@ -1,8 +1,30 @@
 import json
 
+from .models import UserRequest, UserIP
 from .forms import TestForm
+from .utils import save_request
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def get_requests(request):
+    requests = UserRequest.objects.filter(ip__ip=get_client_ip(request)).values('email', 'password',
+                                                                                'first_name', 'last_name')
+    output = json.dumps({'requests': list(requests)}, indent=4)
+    content = {
+        'json_response': output,
+        'status': 'HTTP 200 OK',
+    }
+    return render(request, 'my_form.html', content, status=200)
 
 
 @csrf_exempt
@@ -10,7 +32,11 @@ def post_requests(request):
     if request.method == 'POST':
         form = TestForm(request.POST)
         if form.is_valid():
-            output = json.dumps(form.cleaned_data, indent=4)
+            data = form.cleaned_data
+            user_ip, created = UserIP.objects.get_or_create(ip=get_client_ip(request))
+            save_request(user_ip, data)
+
+            output = json.dumps(data, indent=4)
             content = {
                 'form': form,
                 'json_response': output,
